b4ea259ce8d14390cec368a6d5d84f45727ee696
vaibhavp029 <vaibhavpanchal029@gmail.com>
2026-03-26 02:07:18 +0530

    Initial commit

---
+++ D1/sum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class sum{
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("*** SUM PROGRAM ***\n\n" + "Enter number 1: ");
+		int a = sc.nextInt();
+
+		System.out.println("Enter number 2: ");
+		int b = sc.nextInt();
+
+		int c = a + b;
+		System.out.println("\n\nSUM: " + c);		
+
+}
+}
