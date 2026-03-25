5d3272406c43d991b1c325838ec17119cb140f3c
vaibhavp029 <vaibhavpanchal029@gmail.com>
2026-03-26 02:37:28 +0530

    D1 commit 2

---
+++ D1/swap.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class swap{
+	public static void main(String[] args){
+
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.print("*** SWAP ***\n\nEnter first integer: ");
+	int a = sc.nextInt();
+	System.out.print("Enter second integer: ");
+	int b = sc.nextInt();
+	
+	System.out.println("\nBefore Swapping:\na: " + a + "\nb: " + b);
+
+	a = a + b;
+	b = a - b;
+	a = a - b;
+
+	System.out.println("\nAfter Swapping:\na: "+ a + "\nb: " + b);	
+	
+}
+}
